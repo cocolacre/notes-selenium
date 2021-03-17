97e4ed4c952c630b710d2dcbe27b32c3e58c1e96
Vadim Smirnov <cocolacre@gmail.com>
2021-03-17 15:45:27 +0300

    Add files via upload

---
+++ stepik_1.6_2.py
@@ -0,0 +1,20 @@
+#find_element_by_class_name
+#find_element_by_name
+#find_element_by_id
+#find_element_by_xpath
+#find_element_by_tag_name
+#find_element_by_css_selector
+#find_element_by_link_text
+#find_element_by_partial_link_text
+
+
+#find_element(By.ID, "pattern")
+#find_element(By.CSS_SELECTOR, "pattern")
+#find_element(By.NAME, "pattern")
+#find_element(By.CLASS_NAME, "pattern")
+#find_element(By.LINK_TEXT, "pattern")
+#find_element(By.PARTIAL_LINK_TEXT, "pattern")
+#find_element(By.TAG_NAME, "pattern")
+#find_element(By.XPATH, "pattern")
+
+#THESE ONLY RETURN FIRST FOUND ELEMENT.
